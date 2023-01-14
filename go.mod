ae6ae89136b5e58504bc440106768f52b322147b
Morgan Gallant <morgan@morgangallant.com>
2023-01-14 18:38:19 -0500

    integrated hashicorp/raft, working w/ tailscale inet
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module mgd
 go 1.19
 
 require (
+	github.com/cockroachdb/pebble v0.0.0-20230113195140-25d2b70d1aeb
+	github.com/hashicorp/raft v1.3.11
 	github.com/joho/godotenv v1.4.0
 	github.com/pkg/errors v0.9.1
 	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db
+	golang.org/x/sync v0.1.0
 	tailscale.com v1.34.2
 )
 
@@ -13,9 +16,11 @@ replace tailscale.com => ./../tailscale
 
 require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/akutz/memconn v0.1.0 // indirect
 	github.com/alexbrainman/sspi v0.0.0-20210105120005-909beea2cc74 // indirect
+	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.11.2 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.11.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.6.4 // indirect
@@ -28,13 +33,26 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.6.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.11.1 // indirect
 	github.com/aws/smithy-go v1.9.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cockroachdb/errors v1.8.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
 	github.com/coreos/go-iptables v0.6.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/godbus/dbus/v5 v5.0.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.0.1 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/hashicorp/go-hclog v0.9.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/hdevalence/ed25519consensus v0.0.0-20220222234857-c00d1f31bab3 // indirect
 	github.com/illarion/gonotify v1.0.1 // indirect
 	github.com/insomniacslk/dhcp v0.0.0-20221215072855-de60144f33f8 // indirect
@@ -43,11 +61,19 @@ require (
 	github.com/jsimonetti/rtnetlink v1.1.2-0.20220408201609-d380b505068b // indirect
 	github.com/klauspost/compress v1.15.4 // indirect
 	github.com/kortschak/wol v0.0.0-20200729010619-da482cc4850a // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mdlayher/genetlink v1.2.0 // indirect
 	github.com/mdlayher/netlink v1.6.0 // indirect
 	github.com/mdlayher/sdnotify v1.0.0 // indirect
 	github.com/mdlayher/socket v0.2.3 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.12.2 // indirect
+	github.com/prometheus/client_model v0.2.1-0.20210607210712-147c58e9608a // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rogpeppe/go-internal v1.8.1-0.20211023094830-115ce09fd6b4 // indirect
 	github.com/tailscale/certstore v0.1.1-0.20220316223106-78d6e1c49d8d // indirect
 	github.com/tailscale/golang-x-crypto v0.0.0-20221102133106-bc99ab8c2d17 // indirect
 	github.com/tailscale/goupnp v1.0.1-0.20210804011211-c64d0f06ea05 // indirect
@@ -63,7 +89,6 @@ require (
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/mod v0.6.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.3.1-0.20221220025402-2204b6615fb8 // indirect
 	golang.org/x/term v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
@@ -71,6 +96,7 @@ require (
 	golang.org/x/tools v0.2.0 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20211104114900-415007cec224 // indirect
 	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gvisor.dev/gvisor v0.0.0-20221203005347-703fd9b7fbc0 // indirect
 	inet.af/peercred v0.0.0-20210906144145-0893ea02156a // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
--- mgd.go
+++ mgd.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
+	"encoding/json"
+	"io"
 	"log"
 	"math/rand"
+	"net"
 	"net/netip"
 	"os"
 	"os/signal"
@@ -12,9 +16,12 @@ import (
 	"sync"
 	"time"
 
+	"github.com/cockroachdb/pebble"
+	"github.com/hashicorp/raft"
 	"github.com/joho/godotenv"
 	"github.com/pkg/errors"
 	"golang.org/x/exp/slices"
+	"golang.org/x/sync/errgroup"
 	"tailscale.com/ipn"
 	"tailscale.com/ipn/ipnstate"
 	"tailscale.com/tailcfg"
@@ -37,23 +44,37 @@ func main() {
 		cancel()
 	}()
 	if err := run(ctx); err != nil && !errors.Is(err, context.Canceled) {
-		log.Fatalf("top-level error: %+v", err)
+		log.Fatalf("%+v", err)
 	}
 }
 
 func run(ctx context.Context) error {
-	conn, err := newInetConn(ctx, "mgd")
+	datadir := filepath.Join(os.TempDir(), strconv.FormatInt(time.Now().UnixNano(), 10))
+	if err := os.MkdirAll(datadir, 0755); err != nil {
+		return errors.WithStack(err)
+	}
+	defer os.RemoveAll(datadir)
+	log.Printf("using datadir %s", datadir)
+
+	conn, err := newInetConn(ctx, "mgd", filepath.Join(datadir, "ts"))
 	if err != nil {
 		return err
 	}
 	defer conn.shutdown()
-	conn.setPeerAddedCb(func(peer *tailcfg.Node) {
-		log.Printf("Discovered peer %s (%v)", peer.StableID, peer.Addresses)
-	})
-	conn.setPeerRemovedCb(func(peer *tailcfg.Node) {
-		log.Printf("Lost peer %s", peer.StableID)
-	})
-	log.Printf("connected to inet, self ips: %v", conn.selfIps)
+	log.Printf("connected to inet as %s, self ips: %v", conn.selfId, conn.selfIps)
+
+	manager, err := newClusterManager(conn, filepath.Join(datadir, "raft"))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := manager.close(ctx); err != nil {
+			log.Printf("error shutting down raft: %v", err)
+		}
+	}()
+	log.Printf("started cluster manager")
 
 	<-ctx.Done()
 	return nil
@@ -61,28 +82,33 @@ func run(ctx context.Context) error {
 
 type inetConn struct {
 	inner    *tsnet.Server
+	selfId   tailcfg.StableNodeID
 	selfIps  []netip.Addr
 	selfTags []string
 
 	mu            sync.Mutex // Protects below field(s).
-	peers         map[string]*tailcfg.Node
+	peers         map[tailcfg.StableNodeID]*tailcfg.Node
 	peerAddedCb   func(peer *tailcfg.Node)
 	peerRemovedCb func(peer *tailcfg.Node)
 }
 
-func newInetConn(ctx context.Context, hostname string) (*inetConn, error) {
+func ensureDir(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+func newInetConn(ctx context.Context, hostname, datadir string) (*inetConn, error) {
 	authKey, ok := os.LookupEnv("TSKEY")
 	if !ok {
 		return nil, errors.New("missing TSKEY environment variable")
 	}
 
-	dir := filepath.Join(os.TempDir(), strconv.FormatInt(time.Now().UnixNano(), 10))
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return nil, errors.WithStack(err)
-	}
-
 	ic := &inetConn{
-		peers: make(map[string]*tailcfg.Node),
+		peers: make(map[tailcfg.StableNodeID]*tailcfg.Node),
 	}
 	ic.mu.Lock()
 	defer ic.mu.Unlock()
@@ -96,7 +122,7 @@ func newInetConn(ctx context.Context, hostname string) (*inetConn, error) {
 		defer ic.mu.Unlock()
 
 		existing := ic.peers
-		ic.peers = make(map[string]*tailcfg.Node, len(n.NetMap.Peers))
+		ic.peers = make(map[tailcfg.StableNodeID]*tailcfg.Node, len(n.NetMap.Peers))
 		for _, p := range n.NetMap.Peers {
 			if p.Online == nil || !*p.Online {
 				continue
@@ -106,9 +132,9 @@ func newInetConn(ctx context.Context, hostname string) (*inetConn, error) {
 			if !slices.Equal(cloned.Tags, ic.selfTags) {
 				continue
 			}
-			ic.peers[cloned.ID.String()] = cloned
-			if _, ok := existing[cloned.ID.String()]; ok {
-				delete(existing, cloned.ID.String())
+			ic.peers[cloned.StableID] = cloned
+			if _, ok := existing[cloned.StableID]; ok {
+				delete(existing, cloned.StableID)
 			} else if ic.peerAddedCb != nil {
 				ic.peerAddedCb(cloned)
 			}
@@ -123,9 +149,13 @@ func newInetConn(ctx context.Context, hostname string) (*inetConn, error) {
 		return true
 	}
 
+	if err := ensureDir(datadir); err != nil {
+		return nil, err
+	}
+
 	inner := &tsnet.Server{
 		Hostname:   hostname,
-		Dir:        dir,
+		Dir:        datadir,
 		AuthKey:    authKey,
 		Ephemeral:  true,
 		Logf:       func(format string, args ...any) {},
@@ -146,11 +176,12 @@ func newInetConn(ctx context.Context, hostname string) (*inetConn, error) {
 		}
 	}
 
-	tags := status.Self.Tags.AsSlice()
-	slices.Sort(tags)
-
 	ic.inner = inner
 	ic.selfIps = status.TailscaleIPs
+	ic.selfId = status.Self.ID
+
+	tags := status.Self.Tags.AsSlice()
+	slices.Sort(tags)
 	ic.selfTags = tags
 
 	return ic, nil
@@ -170,4 +201,506 @@ func (c *inetConn) setPeerRemovedCb(cb func(peer *tailcfg.Node)) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.peerRemovedCb = cb
+}
+
+func (c *inetConn) listen(network, addr string) (net.Listener, error) {
+	return c.inner.Listen(network, addr)
+}
+
+func (c *inetConn) dialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	return c.inner.Dial(ctx, network, address)
+}
+
+func (c *inetConn) getPeer(id tailcfg.StableNodeID, clone bool) *tailcfg.Node {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if peer, ok := c.peers[id]; ok {
+		if clone {
+			return peer.Clone()
+		}
+		return peer
+	}
+	return nil
+}
+
+type clusterFsm struct{}
+
+var _ raft.FSM = (*clusterFsm)(nil)
+
+func (fsm *clusterFsm) Apply(l *raft.Log) interface{} {
+	return nil
+}
+
+func (fsm *clusterFsm) Snapshot() (raft.FSMSnapshot, error) {
+	return &clusterFsmSnapshot{}, nil
+}
+
+func (fsm *clusterFsm) Restore(_ io.ReadCloser) error {
+	return nil
+}
+
+type clusterFsmSnapshot struct{}
+
+var _ raft.FSMSnapshot = (*clusterFsmSnapshot)(nil)
+
+func (s *clusterFsmSnapshot) Persist(_ raft.SnapshotSink) error {
+	return nil
+}
+
+func (s *clusterFsmSnapshot) Release() {}
+
+type clusterStore struct {
+	inner store
+}
+
+func (cs *clusterStore) Set(key, val []byte) error {
+	return cs.inner.put(context.Background(), key, val)
+}
+
+func (cs *clusterStore) Get(key []byte) ([]byte, error) {
+	return cs.inner.get(context.Background(), key)
+}
+
+func (cs *clusterStore) SetUint64(key []byte, val uint64) error {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], val)
+	return cs.inner.put(context.Background(), key, buf[:])
+}
+
+func (cs *clusterStore) GetUint64(key []byte) (uint64, error) {
+	val, err := cs.inner.get(context.Background(), key)
+	if err != nil {
+		if errors.Is(err, errNotFound) {
+			return 0, nil
+		}
+		return 0, err
+	}
+	return binary.BigEndian.Uint64(val), nil
+}
+
+const logPrefix = "log:"
+
+func keyForLog(idx uint64) []byte {
+	var buf [len(logPrefix) + 8]byte
+	copy(buf[:], logPrefix)
+	binary.BigEndian.PutUint64(buf[len(logPrefix):], idx)
+	return buf[:]
+}
+
+func (cs *clusterStore) FirstIndex() (uint64, error) {
+	start, end := prefixedRange([]byte(logPrefix))
+	iter, err := cs.inner.newIter(context.Background(), start, end)
+	if err != nil {
+		return 0, err
+	}
+	defer iter.close()
+
+	if !iter.first() {
+		return 0, nil
+	}
+
+	return binary.BigEndian.Uint64(iter.key()[len(logPrefix):]), nil
+}
+
+func (cs *clusterStore) LastIndex() (uint64, error) {
+	start, end := prefixedRange([]byte(logPrefix))
+	iter, err := cs.inner.newIter(context.Background(), start, end)
+	if err != nil {
+		return 0, err
+	}
+	defer iter.close()
+
+	if !iter.last() {
+		return 0, nil
+	}
+	return binary.BigEndian.Uint64(iter.key()[len(logPrefix):]), nil
+}
+
+func (cs *clusterStore) GetLog(idx uint64, log *raft.Log) error {
+	val, err := cs.inner.get(context.Background(), keyForLog(idx))
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(val, log); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (cs *clusterStore) StoreLog(log *raft.Log) error {
+	buf, err := json.Marshal(log)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return cs.inner.put(context.Background(), keyForLog(log.Index), buf)
+}
+
+func (cs *clusterStore) StoreLogs(logs []*raft.Log) error {
+	for _, log := range logs {
+		if err := cs.StoreLog(log); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (cs *clusterStore) DeleteRange(min, max uint64) error {
+	if err := deleteRange(cs.inner, keyForLog(min), keyForLog(max)); err != nil {
+		return err
+	}
+	return nil
+}
+
+var (
+	_ raft.LogStore    = (*clusterStore)(nil)
+	_ raft.StableStore = (*clusterStore)(nil)
+)
+
+const clusterPort = "11106"
+
+type clusterManager struct {
+	selfId        tailcfg.StableNodeID
+	inner         *raft.Raft
+	fsm           *clusterFsm
+	errGroup      *errgroup.Group
+	shutdownFuncs []func(context.Context) error
+}
+
+func newClusterManager(
+	conn *inetConn,
+	datadir string,
+) (*clusterManager, error) {
+	manager := &clusterManager{
+		fsm:      &clusterFsm{},
+		errGroup: new(errgroup.Group),
+	}
+
+	pebble, err := newPebbleStore(filepath.Join(datadir, "store"), true)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	manager.shutdownFuncs = append(manager.shutdownFuncs, func(_ context.Context) error {
+		return pebble.close()
+	})
+	store := &clusterStore{inner: pebble}
+
+	snapshots, err := raft.NewFileSnapshotStore(datadir, 2, os.Stderr)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if len(conn.selfIps) == 0 {
+		return nil, errors.New("no self ip")
+	}
+	serverAddr := net.JoinHostPort(conn.selfIps[0].String(), clusterPort)
+
+	streamLayer, err := newInetStreamLayer(conn, clusterPort)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	manager.shutdownFuncs = append(manager.shutdownFuncs, func(ctx context.Context) error {
+		return streamLayer.Close()
+	})
+
+	transport := raft.NewNetworkTransportWithConfig(&raft.NetworkTransportConfig{
+		ServerAddressProvider: &inetAddressProvider{conn},
+		Stream:                streamLayer,
+		MaxPool:               3,
+		Timeout:               10 * time.Second,
+	})
+	manager.shutdownFuncs = append(manager.shutdownFuncs, func(ctx context.Context) error {
+		return transport.Close()
+	})
+
+	conf := raft.DefaultConfig()
+	conf.LogLevel = "warn"
+	conf.LocalID = raft.ServerID(conn.selfId)
+
+	manager.inner, err = raft.NewRaft(conf, manager.fsm, store, store, snapshots, transport)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	manager.shutdownFuncs = append(manager.shutdownFuncs, func(ctx context.Context) error {
+		future := manager.inner.Shutdown()
+		return future.Error()
+	})
+
+	conn.setPeerAddedCb(manager.peerAdded)
+	conn.setPeerRemovedCb(manager.peerRemoved)
+
+	obsChan := manager.startObservationProcessor()
+	observer := raft.NewObserver(obsChan, true, nil)
+	manager.inner.RegisterObserver(observer)
+	manager.shutdownFuncs = append(manager.shutdownFuncs, func(ctx context.Context) error {
+		manager.inner.DeregisterObserver(observer)
+		return nil
+	})
+
+	servers := make([]raft.Server, 0, len(conn.peers)+1)
+	servers = append(servers, raft.Server{
+		ID:      raft.ServerID(conn.selfId),
+		Address: raft.ServerAddress(serverAddr),
+	})
+	for _, peer := range conn.peers {
+		if len(peer.Addresses) == 0 {
+			continue
+		}
+		servers = append(servers, raft.Server{
+			ID: raft.ServerID(peer.StableID),
+			Address: raft.ServerAddress(
+				net.JoinHostPort(peer.Addresses[0].Addr().String(), clusterPort),
+			),
+		})
+	}
+
+	manager.inner.BootstrapCluster(raft.Configuration{
+		Servers: servers,
+	})
+
+	return manager, nil
+}
+
+type inetStreamLayer struct {
+	net.Listener
+	inner *inetConn
+}
+
+func newInetStreamLayer(inner *inetConn, port string) (*inetStreamLayer, error) {
+	lis, err := inner.listen("tcp", ":"+port)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return &inetStreamLayer{
+		Listener: lis,
+		inner:    inner,
+	}, nil
+}
+
+func (isl *inetStreamLayer) Dial(
+	address raft.ServerAddress,
+	timeout time.Duration,
+) (net.Conn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	return isl.inner.dialContext(ctx, "tcp", string(address))
+}
+
+type inetAddressProvider struct {
+	inner *inetConn
+}
+
+func (iap *inetAddressProvider) ServerAddr(id raft.ServerID) (raft.ServerAddress, error) {
+	if id == raft.ServerID(iap.inner.selfId) {
+		return raft.ServerAddress(net.JoinHostPort(iap.inner.selfIps[0].String(), clusterPort)), nil
+	}
+	peer := iap.inner.getPeer(tailcfg.StableNodeID(id), false)
+	if peer == nil {
+		return "", errors.New("peer not found")
+	} else if len(peer.Addresses) == 0 {
+		return "", errors.New("peer has no addresses")
+	}
+	return raft.ServerAddress(net.JoinHostPort(peer.Addresses[0].Addr().String(), clusterPort)), nil
+}
+
+var _ raft.StreamLayer = (*inetStreamLayer)(nil)
+
+func (cm *clusterManager) close(ctx context.Context) error {
+	if cm.inner.State() == raft.Leader {
+		if err := cm.inner.RemoveServer(raft.ServerID(cm.selfId), 0, time.Second*5).Error(); err != nil {
+			return err
+		}
+	}
+	for i := len(cm.shutdownFuncs) - 1; i >= 0; i-- {
+		if err := cm.shutdownFuncs[i](ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (cm *clusterManager) peerAdded(peer *tailcfg.Node) {
+	log.Printf("discovered peer %s (%v)", peer.StableID, peer.Addresses)
+	if len(peer.Addresses) > 0 && cm.inner.State() == raft.Leader {
+		endpoint := net.JoinHostPort(peer.Addresses[0].Addr().String(), clusterPort)
+		log.Printf("adding voter %s (%s)", peer.StableID, endpoint)
+		if err := cm.inner.AddVoter(raft.ServerID(peer.StableID), raft.ServerAddress(endpoint), 0, 0).Error(); err != nil {
+			log.Printf("failed to add voter %s to cluster: %v", peer.StableID, err)
+		}
+	}
+}
+
+func (cm *clusterManager) peerRemoved(peer *tailcfg.Node) {
+	log.Printf("lost peer %s", peer.StableID)
+	if cm.inner.State() == raft.Leader {
+		if err := cm.inner.RemoveServer(raft.ServerID(peer.StableID), 0, 0).Error(); err != nil {
+			log.Printf("failed to remove server %s from cluster: %v", peer.StableID, err)
+		}
+	}
+}
+
+func (cm *clusterManager) startObservationProcessor() chan raft.Observation {
+	c := make(chan raft.Observation)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cm.errGroup.Go(func() error {
+		defer cancel()
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case o := <-c:
+				cm.processObservation(o)
+			}
+		}
+	})
+
+	cm.shutdownFuncs = append(cm.shutdownFuncs, func(_ context.Context) error {
+		cancel()
+		return nil
+	})
+
+	return c
+}
+
+func (cm *clusterManager) processObservation(o raft.Observation) {
+	leaderObservation, ok := o.Data.(raft.LeaderObservation)
+	if !ok {
+		return
+	}
+	log.Printf(
+		"observed leader as %s (%s)",
+		leaderObservation.LeaderID,
+		leaderObservation.LeaderAddr,
+	)
+}
+
+type iterator interface {
+	next() bool
+	valid() bool
+	first() bool
+	last() bool
+	key() []byte
+	value() []byte
+	close() error
+}
+
+type store interface {
+	close() error
+	get(ctx context.Context, key []byte) ([]byte, error)
+	put(ctx context.Context, key, value []byte) error
+	delete(ctx context.Context, key []byte) error
+	newIter(ctx context.Context, start, end []byte) (iterator, error)
+}
+
+var errNotFound = errors.New("store: not found")
+
+type pebbleStore struct {
+	db         *pebble.DB
+	syncWrites bool
+}
+
+var _ store = (*pebbleStore)(nil)
+
+func newPebbleStore(datadir string, syncWrites bool) (*pebbleStore, error) {
+	db, err := pebble.Open(datadir, &pebble.Options{})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return &pebbleStore{
+		db:         db,
+		syncWrites: syncWrites,
+	}, nil
+}
+
+func (s *pebbleStore) close() error {
+	return s.db.Close()
+}
+
+func (s *pebbleStore) get(_ context.Context, key []byte) ([]byte, error) {
+	value, closer, err := s.db.Get(key)
+	if err != nil {
+		if errors.Is(err, pebble.ErrNotFound) {
+			return nil, errNotFound
+		}
+		return nil, errors.WithStack(err)
+	}
+	defer closer.Close()
+
+	buf := make([]byte, len(value))
+	copy(buf, value)
+
+	return buf, nil
+}
+
+func (s *pebbleStore) put(_ context.Context, key, value []byte) error {
+	opts := pebble.NoSync
+	if s.syncWrites {
+		opts = pebble.Sync
+	}
+	if err := s.db.Set(key, value, opts); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (s *pebbleStore) delete(_ context.Context, key []byte) error {
+	opts := pebble.NoSync
+	if s.syncWrites {
+		opts = pebble.Sync
+	}
+	if err := s.db.Delete(key, opts); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (s *pebbleStore) newIter(_ context.Context, start, end []byte) (iterator, error) {
+	iter := s.db.NewIter(&pebble.IterOptions{
+		LowerBound: start,
+		UpperBound: end,
+	})
+	return &pebbleIter{iter: iter}, nil
+}
+
+type pebbleIter struct {
+	iter *pebble.Iterator
+}
+
+var _ iterator = (*pebbleIter)(nil)
+
+func (i *pebbleIter) next() bool    { return i.iter.Next() }
+func (i *pebbleIter) valid() bool   { return i.iter.Valid() }
+func (i *pebbleIter) first() bool   { return i.iter.First() }
+func (i *pebbleIter) last() bool    { return i.iter.Last() }
+func (i *pebbleIter) key() []byte   { return i.iter.Key() }
+func (i *pebbleIter) value() []byte { return i.iter.Value() }
+func (i *pebbleIter) close() error  { return i.iter.Close() }
+
+func deleteRange(store store, start, end []byte) error {
+	iter, err := store.newIter(context.Background(), start, end)
+	if err != nil {
+		return err
+	}
+	defer iter.close()
+	for iter.first(); iter.valid(); iter.next() {
+		if err := store.delete(context.Background(), iter.key()); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func prefixedRange(prefix []byte) ([]byte, []byte) {
+	upperBound := func(b []byte) []byte {
+		end := make([]byte, len(b))
+		copy(end, b)
+		for i := len(end) - 1; i >= 0; i-- {
+			end[i] = end[i] + 1
+			if end[i] != 0 {
+				return end[:i+1]
+			}
+		}
+		return nil
+	}
+	return prefix, upperBound(prefix)
 }

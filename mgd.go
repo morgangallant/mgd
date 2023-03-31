9fe55d3798f9882654064acfe55c9056395cb7ed
Morgan Gallant <morgan@morgangallant.com>
2023-03-30 23:19:31 -0400

    Update mgd.go

--- mgd.go
+++ mgd.go
@@ -29,7 +29,7 @@ import (
 	"tailscale.com/tsnet"
 )
 
-func init() { // This is a cool comment. Here's another one! And another? One more time!
+func init() { // This is a cool comment. Here's another one! And another? One more time! And another!
 	rand.Seed(time.Now().UnixNano())
 	_ = godotenv.Load()
 
